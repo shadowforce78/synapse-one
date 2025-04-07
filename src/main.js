9f6e5408ed3255bad0e594e31e0bdf9cf035dbe4
shadowforce78 <planque.adam@gmail.com>
2025-04-07 23:32:15 +0200

    Inital commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('client/dist/index.html');
+}
+
+app.whenReady().then(createWindow);
